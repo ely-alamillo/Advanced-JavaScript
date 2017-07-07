de05e772e4f4574e0a4f819e14d6cd4b5f0e9c95
Ely Alamillo <el0013@gmail.com>
2017-07-07 17:15:09 -0500

    finished and passed tests

--- src/es6.js
+++ src/es6.js
@@ -7,50 +7,101 @@
 //----------------
 // const, =>, default parameters, arrow functions default return statements using ()
 
-var food = 'pineapple';
+// ------------------------------------------------ this is the es5 example ---
+// var food = 'pineapple';
+//
+// var isMyFavoriteFood = function(food) {
+//   food = food || 'thousand-year-old egg'; //This sets a default value if `food` is falsey
+//   return food === 'thousand-year-old egg';
+// };
+//
+// var isThisMyFavorite = isMyFavoriteFood(food = 'thousand-year-old egg');
+// console.log(isThisMyFavorite);
 
-var isMyFavoriteFood = function(food) {
-  food = food || 'thousand-year-old egg'; //This sets a default value if `food` is falsey
-  return food === 'thousand-year-old egg';
-};
-
-var isThisMyFavorite = isMyFavoriteFood(food);
-
-//----------------
-//const, class, template literals, enhanced object literals (foo: foo, -> foo,)
-
-var User = function(options) {
-  this.username = options.username;
-  this.password = options.password;
-  this.sayHi = function() {
-    return this.username + ' says hello!';
-  };
+// ----------------------------------------------- this is converted to es6 ---
+const food = 'pineapple';
+const isMyFavoriteFood = (food) => {
+  return (!food);
 }
 
-var username = 'JavaScriptForever';
-var password = 'password';
+const isThisMyFavorite = isMyFavoriteFood(food);
 
-var me = new User({
+// ------------------------------------------------ this is the es5 example ---
+//const, class, template literals, enhanced object literals (foo: foo, -> foo,)
+
+// --- this is the es5 example ---
+// var User = function(options) {
+//   this.username = options.username;
+//   this.password = options.password;
+//   this.sayHi = function() {
+//     return this.username + ' says hello!';
+//   };
+// }
+//
+// var username = 'JavaScriptForever';
+// var password = 'password';
+//
+// var me = new User({
+//   username: username,
+//   password: password,
+// });
+
+// ----------------------------------------------- this is converted to es6 ---
+class User {
+  constructor(options) {
+    this.username = options.username;
+    this.password = options.password;
+  }
+  sayHi() {
+    return `${this.username} says hello!`
+  }
+}
+
+const username = 'JavaScriptForever';
+const password = 'password';
+
+const me = new User({
   username: username,
   password: password,
 });
+// console.log(me.sayHi());
+// console.log(me.sayHi());
 
 // ----------------
+// ------------------------------------------------ this is the es5 example ---
 // let, const, =>, ... (spread operator)
 
-var addArgs = function () {
-  var sum = 0;
-  for (var i = 0; i < arguments.length; i++) {
-    sum += arguments[i];
-  }
+// var addArgs = function () {
+//   var sum = 0;
+//   for (var i = 0; i < arguments.length; i++) {
+//     sum += arguments[i];
+//   }
+//   return sum;
+// };
+//
+// var argsToCb = function (cb) {
+//   var args = Array.prototype.slice.call(arguments);
+//   return cb.apply(null, args.splice(1));
+// };
+//
+// var result = argsToCb(addArgs, 1, 2, 3, 4, 5); //result should be 15
+
+// ----------------------------------------------- this is converted to es6 ---
+const addArgs = (args) => {
+  let sum = 0;
+  args.forEach((element) => {
+    sum += element
+  });
   return sum;
 };
 
-var argsToCb = function (cb) {
-  var args = Array.prototype.slice.call(arguments);
-  return cb.apply(null, args.splice(1));
+const argsToCb = (cb, ...args) => {
+  const argsArray = args;
+  return cb(argsArray);
 };
 
-var result = argsToCb(addArgs, 1, 2, 3, 4, 5); //result should be 15
+const result = argsToCb(addArgs, 1, 2, 3, 4, 5);
+console.log(result);
+
 
 /* eslint-enable */
